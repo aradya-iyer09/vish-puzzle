1993806c9e43312d660775b2ba5bf2b9d30c31c3
aradya-iyer09 <145424663+aradya-iyer09@users.noreply.github.com>
2024-09-03 11:29:34 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      question: "Aryabhata (Geometry):If the circumference of a circle is 44 units, what is the diameter?",
+      answer: "14 units",
+      options: [
+        "10 units",
+        "14 units",
+        "4 units",
+        "12 units"
+      ]
+    },
+    {
+      numb: 2,
+      question: "Varahamihira (Number Theory):What is the sum of the first 5 prime numbers?",
+      answer: "28",
+      options: [
+        "15",
+        "27",
+        "20",
+        "28"
+      ]
+    },
+  
+    {
+      numb: 3,
+      question: "Mahavira (Fractions):Simplify: 45/60 ?",
+      answer: "3/4",
+      options: [
+        "5/10",
+        "3/5",
+        "3/4",
+        "4/8"
+      ]
+    },
+      {
+      numb: 4,
+      question: "Aryabhata (Arithmetic):If you divide 100 by 8, what is the remainder?",
+      answer: "4",
+      options: [
+        "5",
+        "2",
+        "4",
+        "6"
+      ]
+    },
+    {
+        numb: 5,
+        question: "Bhaskara I (Sine Function):What is sin 0°?",
+        answer: "0",
+        options: [
+          "1",
+          "1.5",
+          "0",
+          "0.5"
+        ]
+      } ,
+      
+  ];
